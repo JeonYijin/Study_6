e6822932747eb2af6caa5b424995159944da2638
JeonYijin <juan6016@naver.com>
2021-07-22 17:44:59 +0900

    7.22-1

--- src/com/ae/ae2/S2Main.java
+++ src/com/ae/ae2/S2Main.java
@@ -3,8 +3,11 @@ package com.ae.ae2;
 public class S2Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
+		
+		
 	}
 
 }
--- src/com/ae/ae3/S3Main.java
+++ src/com/ae/ae3/S3Main.java
@@ -3,8 +3,7 @@ package com.ae.ae3;
 public class S3Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
